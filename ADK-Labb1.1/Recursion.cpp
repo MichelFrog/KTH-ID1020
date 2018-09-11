740bc8a8ada3bf1b2789f33493443d9d85eb5cc1
Michel Frog <m.quadria@gmail.com>
2018-09-11 18:13:22 +0200

    added left out assigment
    Added C assigment that was forgetten

---
+++ ADK-Labb1.1/Recursion.cpp
@@ -0,0 +1,12 @@
+#include "Recursion.h"
+
+
+
+Recursion::Recursion()
+{
+}
+
+Recursion::~Recursion()
+{
+}
+
---
+++ ADK-Labb1.1/Recursion.h
@@ -0,0 +1,12 @@
+#ifndef RECURSION_H
+#define RECURSION_H
+
+class Recursion
+{
+public:
+    Recursion();
+    ~Recursion();
+
+};
+
+#endif // RECURSION_H
